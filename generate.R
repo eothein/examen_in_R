d493eb66f3a2790bb98824d73bee3dac918d0304
Jens Buysse <jens.buysse@gmail.com>
2020-04-16 16:35:06 +0200

    [feat] Change style of opgave

--- generate.R
+++ generate.R
@@ -1,6 +1,6 @@
 ## Script om de examens te genereren
 
-## Creeër een examen met een aantal vragen.
+## Cree??r een examen met een aantal vragen.
 # De vector die je als argument meegeeft bevat de mogelijke vragen 
 ## voor het examen
 # Indien je een random vraag wil uit een selectie, moet je die meegeven als vector als element in de lijst
@@ -21,7 +21,10 @@ myexam <- list(
 ## De templates voor het examen staan in de directory templates
 exams2pdf(myexam, n = 1, name = c("pdf-examen", "pdf-oplossing"),
           encoding = "UTF-8",
+          showpoints = "TRUE",
           dir = "output",
           edir = "oefeningen",
+          inputs = "/Users/eothein/education/examentest/templates/hogent-examen.sty",
           template = c("templates/opgave.tex", "templates/oplossing.tex"),
          )
+
